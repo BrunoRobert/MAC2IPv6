4c8f40d1b5bdaf890751a45a0a1c6bb8e4d8fc10
Frank Weichbrodt <frank.weichbrodt@web.de>
2016-11-13 08:48:52 +0100

    2nd gateway

--- mac2ipv6.php
+++ mac2ipv6.php
@@ -4,6 +4,7 @@
   <meta charset="ISO-8859-1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ffhgw | MAC to IPv6</title>
+  <style>body {font-family: Helvetica Neue,Helvetica,Arial,sans-serif;font-size: 14px;}</style>
  </head>
 <body>
 
@@ -17,7 +18,8 @@
 // Wurde Variable übergeben?
 if (isset($_POST['mac']) && $_POST['mac'] != "") {
  $mac = $_POST['mac'];
- $prefix = "2a01:a380:8408:2000:"; // Prefix der Community
+ $prefix1 = "2a01:a380:8408:2000:";
+ $prefix2 = "2a03:2260:200e:0000:";
 
  $mac_array = explode(":", $mac);
 
@@ -27,13 +29,22 @@ if (isset($_POST['mac']) && $_POST['mac'] != "") {
   // bit flip
   $mac_array[0] = dechex(hexdec($mac_array[0]) ^ 2);
 
-  // IPv6 zusammensetzen
-  $ipv6  = $prefix;
+  // quick and dirty für 2 Gateways
+  // ipv6 zusammensetzen (1. Gateway)
+  $ipv6  = $prefix1;
   $ipv6 .= $mac_array[0] . $mac_array[1] . ":" . $mac_array[2] . "ff:fe" . $mac_array[3] . ":" . $mac_array[4] . $mac_array[5];
   $ipv6  = strtolower($ipv6);
 
   // Link
-  echo strtoupper($mac) ." &rarr; <a href='http://[".$ipv6."]' target='_blank'>[".$ipv6."]</a>";
+  echo "<b>steinbeckertor</b><br />". strtoupper($mac) ." &rarr; <a title='steinbeckertor' href='http://[".$ipv6."]' target='_blank'>[".$ipv6."]</a><br /><br />";
+
+  // ipv6 zusammensetzen (2. Gateway)
+  $ipv6  = $prefix2;
+  $ipv6 .= $mac_array[0] . $mac_array[1] . ":" . $mac_array[2] . "ff:fe" . $mac_array[3] . ":" . $mac_array[4] . $mac_array[5];
+  $ipv6  = strtolower($ipv6);
+
+  // Link
+  echo "<b>muehlentor</b><br />". strtoupper($mac) ." &rarr; <a title='muehlentor' href='http://[".$ipv6."]' target='_blank'>[".$ipv6."]</a>";
 
  } else {
 
